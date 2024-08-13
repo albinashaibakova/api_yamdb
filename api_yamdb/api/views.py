fc3e720aab4dc6da98c33c9f8d215bdf48198c40
Ekaterina Korolevskaia <matissiya@gmail.com>
2024-08-13 20:42:22 +0300

    add genres serializers and views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,22 @@
-from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework import filters, permissions
 
-from reviews.models import Category
-from .serializers import CategorySerializer
+from reviews.models import Category, Genre
+from .mixins import ListCreateDestroyViewSet
+from .serializers import CategorySerializer, GenreSerializer
 
 
-class CategoryViewSet(mixins.ListModelMixin,
-                      mixins.CreateModelMixin,
-                      mixins.DestroyModelMixin,
-                      viewsets.GenericViewSet):
+class CategoryViewSet(ListCreateDestroyViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny,)
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
