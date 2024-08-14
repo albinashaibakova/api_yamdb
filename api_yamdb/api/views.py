41a8a49efa05c75de6d3d885206c1f153565ad6b
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-14 17:24:27 +0500

    added user get token viewset

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,40 +1,53 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import (
-    filters,
-    mixins,
-    permissions,
-    status,
-    viewsets
-)
+from django.shortcuts import get_object_or_404
+from rest_framework import (filters, mixins, permissions,
+                            status, viewsets)
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import AccessToken
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, CustomUser, Genre, Title
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
-from .permissions import IsAdminOrReadOnly
-from .serializers import (
-    CategorySerializer,
-    GenreSerializer,
-    TitleSerializer,
-    TitleListSerializer,
-    UserSignUpSerializer
-)
-
-User = get_user_model()
+from .serializers import (CategorySerializer,
+                          GenreSerializer,
+                          TitleSerializer,
+                          TitleListSerializer,
+                          UserGetTokenSerializer,
+                          UserSignUpSerializer)
+from .utils import send_confirmation_email
 
 
 class UserSignUpViewSet(mixins.CreateModelMixin,
                         viewsets.GenericViewSet):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = UserSignUpSerializer
-    permission_classes = (permissions.AllowAny,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        User.objects.create(**serializer.validated_data)
+        CustomUser.objects.create(**serializer.validated_data)
+        user = CustomUser.objects.get(username=serializer.validated_data['username'])
+        confirmation_code = default_token_generator.make_token(user)
+        send_confirmation_email(email=user.email,
+                                confirmation_code=confirmation_code)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserGetTokenViewSet(mixins.CreateModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserGetTokenSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data.get('username')
+        confirmation_code = serializer.validated_data.get('confirmation_code')
+        user = get_object_or_404(CustomUser, username=username)
+        token = {'token': str(AccessToken.for_user(user))}
+        return Response(token, status=status.HTTP_200_OK)
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
@@ -43,7 +56,7 @@ class CategoryViewSet(ListCreateDestroyViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class GenreViewSet(ListCreateDestroyViewSet):
@@ -52,14 +65,14 @@ class GenreViewSet(ListCreateDestroyViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,6 +12,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -20,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'api',
     'reviews'
@@ -67,6 +71,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -84,6 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 
 LANGUAGE_CODE = 'en-us'
@@ -96,6 +102,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
@@ -105,7 +112,18 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ],
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication', ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'api' / 'sent_emails'
+
+EMAIL_ADDRESS = 'registration@yamb.ru'
