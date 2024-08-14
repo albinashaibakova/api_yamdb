5d65a36d92433ac8978c0d3a029b9d404321421a
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-14 17:31:46 +0500

    fixed user_signup_viewset, auth urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,70 +1,33 @@
-from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import get_object_or_404
-from rest_framework import (
-    filters,
-    mixins,
-    permissions,
-    status,
-    viewsets
-)
-from rest_framework import (
-    filters,
-    mixins,
-    permissions,
-    status,
-    viewsets
-)
+from rest_framework import (filters, mixins, permissions,
+                            status, viewsets)
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Category, Genre, Title
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
-from .permissions import IsAdminOrReadOnly
 from .serializers import (CategorySerializer,
                           GenreSerializer,
                           TitleSerializer,
                           TitleListSerializer,
-                          UserGetTokenSerializer,
                           UserSignUpSerializer)
-from .utils import send_confirmation_email
 
-User = get_user_model()
+
+CustomUser = get_user_model()
 
 
 class UserSignUpViewSet(mixins.CreateModelMixin,
                         viewsets.GenericViewSet):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = UserSignUpSerializer
     permission_classes = (permissions.AllowAny,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        User.objects.create(**serializer.validated_data)
-        user = User.objects.get(username=serializer.validated_data['username'])
-        confirmation_code = default_token_generator.make_token(user)
-        send_confirmation_email(email=user.email,
-                                confirmation_code=confirmation_code)
+        CustomUser.objects.create(**serializer.validated_data)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class UserGetTokenViewSet(mixins.CreateModelMixin,
-                          viewsets.GenericViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserGetTokenSerializer
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        username = serializer.validated_data.get('username')
-        confirmation_code = serializer.validated_data.get('confirmation_code')
-        user = get_object_or_404(User, username=username)
-        token = {'token': str(AccessToken.for_user(user))}
-        return Response(token, status=status.HTTP_200_OK)
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
@@ -73,7 +36,7 @@ class CategoryViewSet(ListCreateDestroyViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class GenreViewSet(ListCreateDestroyViewSet):
@@ -82,14 +45,14 @@ class GenreViewSet(ListCreateDestroyViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (permissions.AllowAny,)
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
