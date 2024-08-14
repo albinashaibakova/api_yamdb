6cf42f1de64c7b9269dcc11d9cc58c87cbcf4715
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-14 17:24:27 +0500

    added user get token viewset

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 from rest_framework import (
     filters,
     mixins,
