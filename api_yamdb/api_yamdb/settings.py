d07c9c40e9ea4d471d77f91648b6d8f1d17d1754
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-14 18:27:15 +0500

    fixed user_signup_viewset, auth urls, added default_permission_classes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,6 +110,8 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
