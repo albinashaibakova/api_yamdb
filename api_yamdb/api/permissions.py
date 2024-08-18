2dab05480cd092771d12b1a3b328d037d8f965eb
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-18 22:00:14 +0500

    pep-8 fixes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,10 +26,8 @@ class IsAuthorAdminModeratorOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.is_moderator
                 or request.user.is_superuser
-                or request.user.is_admin
-        )
+                or request.user.is_admin)
