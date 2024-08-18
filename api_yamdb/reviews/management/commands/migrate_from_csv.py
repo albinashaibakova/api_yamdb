ce84ab73139238311d21367d3112f5695db18d46
Ekaterina Korolevskaia <matissiya@gmail.com>
2024-08-18 19:48:32 +0300

    fix command pep8

--- api_yamdb/reviews/management/commands/migrate_from_csv.py
+++ api_yamdb/reviews/management/commands/migrate_from_csv.py
@@ -1,14 +1,18 @@
 import csv
+from typing import Any, Optional
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.db import models
 
 from api_yamdb.settings import DATA_CSV_DIR
-from reviews.models import *
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
 
 User = get_user_model()
 
-ALLOWED_FILES_DICT = (
+ALLOWED_FILES_DICT: list[
+    tuple[str, Any[models.Model], Optional[dict[str, str]]]
+] = [
     ('category', Category, {}),
     ('genre', Genre, {}),
     ('titles', Title, {}),
@@ -16,9 +20,9 @@ ALLOWED_FILES_DICT = (
     ('users', User, {}),
     ('review', Review, {'title_id': 'title'}),
     ('comments', Comment, {'review_id': 'review'}),
-)
+]
 
-COLUMN_FOREIGN_MODEL = {
+COLUMN_FOREIGN_MODEL: dict[str, models.Model] = {
     'category': Category,
     'title_id': Title,
     'genre_id': Genre,
@@ -35,12 +39,14 @@ def load_data(filename: str, model: models.Model, mappings: dict):
             data = dict(row)
             for key in original.keys():
                 if key in COLUMN_FOREIGN_MODEL:
-                    foreign_model = COLUMN_FOREIGN_MODEL.get(key)
+                    foreign_model: models.Model = COLUMN_FOREIGN_MODEL.get(key)
                     foreign_model_key = key
                     if key in mappings.keys():
                         data.pop(key)
                         foreign_model_key = mappings.get(key)
-                    data[foreign_model_key] = foreign_model.objects.get(pk=original[key])
+                    data[foreign_model_key] = foreign_model.objects.get(
+                        pk=original[key]
+                    )
             if not model.objects.filter(pk=data['id']).exists():
                 model.objects.create(**data)
 
